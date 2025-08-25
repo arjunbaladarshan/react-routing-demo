d54580718f3b13a859402098f08c68713d4c546f
arjunbaladarshan <60723978+arjunbaladarshan@users.noreply.github.com>
2025-08-25 09:29:04 +0530

    React-Routing-Demo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Layout(){
+  return(
+    <>
+      <Link to='/home'>Home</Link> &nbsp;
+      <Link to='/about'>About</Link> &nbsp;
+      <Link to='/aboutcollege'>AboutCollege</Link> &nbsp;
+      <Link to='/contact'>Contact</Link> <br/>
+
+      <Outlet/>
+
+      <br/>
+      <br/>
+      <h5>Thank you</h5>
+      
+    </>
+  );
+}
+
+function Home(){
+  return(
+    <>
+      <h1>This is Home page</h1>
+    </>
+  );
+}
+
+function About(){
+  return(
+    <>
+      <h1>This is About page</h1>
+    </>
+  );
+}
+
+function AboutCollege(){
+  return(
+    <>
+      <h1>This is About-College page</h1>
+    </>
+  );
+}
+
+function Contact(){  
+  return(
+    <>
+      <h1>This is Contact page</h1>
+    </>
+  );
+}
+
+root.render(
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/home" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/aboutcollege" element={<AboutCollege/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>
+);
