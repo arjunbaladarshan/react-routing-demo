1b33b06a8985bea0b00751a88e269bdbd361212d
arjunbaladarshan <60723978+arjunbaladarshan@users.noreply.github.com>
2025-08-27 08:41:18 +0530

    useState Demo

--- src/index.js
+++ src/index.js
@@ -1,70 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Layout(){
+
+
+
+function TestHook(){
+  //this is state
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("arjun");
+  
+  
   return(
     <>
-      <Link to='/home'>Home</Link> &nbsp;
-      <Link to='/about'>About</Link> &nbsp;
-      <Link to='/aboutcollege'>AboutCollege</Link> &nbsp;
-      <Link to='/contact'>Contact</Link> <br/>
-
-      <Outlet/>
-
+      Count = {count}
       <br/>
-      <br/>
-      <h5>Thank you</h5>
-      
+      <button onClick={()=>{
+        setCount(count+1);
+      }}>
+        Increment
+      </button>
     </>
   );
 }
 
-function Home(){
-  return(
-    <>
-      <h1>This is Home page</h1>
-    </>
-  );
-}
-
-function About(){
-  return(
-    <>
-      <h1>This is About page</h1>
-    </>
-  );
-}
-
-function AboutCollege(){
-  return(
-    <>
-      <h1>This is About-College page</h1>
-    </>
-  );
-}
-
-function Contact(){  
-  return(
-    <>
-      <h1>This is Contact page</h1>
-    </>
-  );
-}
 
 root.render(
   <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route path="/home" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/aboutcollege" element={<AboutCollege/>} />
-          <Route path="/contact" element={<Contact/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <TestHook/>
   </>
 );
